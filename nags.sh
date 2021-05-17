31f090b8b43c376dde7d341a13edee0f3c98d257
Andrew Dean <oneandydean@hotmail.com>
2021-05-17 01:12:54 +0100

    Make one file for running all proform system sets to avoid repetition and tidy up this code quite a bit

--- nags.sh
+++ nags.sh
@@ -1,6 +1,2 @@
 mv /e/Users/Andrew/Downloads/Systems.csv ./ferret.csv
 python proform7.py
-python proform6.py
-python proform+.py
-python proform.py
-python bsp9.py
--- proform7.py
+++ proform7.py
@@ -1,14 +1,13 @@
 import csv
 import plotly.graph_objects as go
 
-picks = []
-
 
 class UnwantedSystemError(ValueError):
     """Exception for raising when the system of the given pick is not desired."""
 
 
 class Pick:
+    """Class for representing a pick."""
 
     SYSTEM_TN2 = "TN2"
     SYSTEM_DTR = "DTR"
@@ -18,9 +17,40 @@ class Pick:
     SYSTEM_TJS = "TJS"
     SYSTEM_ACCAS = "ACCAS"
 
-    def __init__(self, column_headers, pick_list):
+    SYSTEM_PACE = "PACE"
+    SYSTEM_EASED = "EASED"
+    SYSTEM_TOP_OF_POWER = "TOP"
+    SYSTEM_TOP_SPEED_AND_JOCKEY = "SnJ"
+    SYSTEM_NTF_CHELTENHAM_1 = "NTF1"
+    SYSTEM_NTF_CHELTENHAM_2 = "NTF2"
+    SYSTEM_DTR_HURDLES_ONLY = "DTRH"
+    SYSTEM_UK_TRAVELLERS = "UKT"
+    SYSTEM_SPTT = "SPTT"
+    SYSTEM_SPTJ = "SPTJ"
 
-        data_columns = {
+    SYSTEMS = {     # Translating from CSV names to nice abbreviations
+        "JR-TN2": SYSTEM_TN2,
+        "JR-DTR": SYSTEM_DTR,
+        "JR-MR3.2": SYSTEM_MR3,
+        "JR - LT6R": SYSTEM_LT6R,
+        "JR-JLT2": SYSTEM_JLT2,
+        "JR - ACCAS": SYSTEM_ACCAS,
+        "tjs": SYSTEM_TJS,
+
+        "jr-pace1": SYSTEM_PACE,
+        "JR-EASED": SYSTEM_EASED,
+        "Top of Power": SYSTEM_TOP_OF_POWER,
+        "Top Speed and Jockey": SYSTEM_TOP_SPEED_AND_JOCKEY,
+        "NTF Cheltenham": SYSTEM_NTF_CHELTENHAM_1,
+        "cheltsLTOstiff": SYSTEM_NTF_CHELTENHAM_2,
+        "DTR Hurdles Only": SYSTEM_DTR_HURDLES_ONLY,
+        "SPTT": SYSTEM_SPTT,
+        "SPTJ": SYSTEM_SPTJ,
+    }
+
+    @classmethod
+    def initialise_data_columns(cls, column_headers):
+        cls.data_columns = {
             "system": column_headers.index("FUTURE_SYSTEM"),
             "horse": column_headers.index("FUTURE_HORSE"),
             "course": column_headers.index("FUTURE_VENUE"),
@@ -28,29 +58,22 @@ class Pick:
             "date": column_headers.index("FUTURE_RACEDATE")
         }
 
-        self.systems = set()
-        system = pick_list[data_columns["system"]]
-        if system == "JR-TN2":
-            self.systems.add(self.SYSTEM_TN2)
-        elif system == "JR-DTR":
-            self.systems.add(self.SYSTEM_DTR)
-        elif system == "JR-MR3.2":
-            self.systems.add(self.SYSTEM_MR3)
-        elif system == "JR - LT6R":
-            self.systems.add(self.SYSTEM_LT6R)
-        elif system == "JR-JLT2":
-            self.systems.add(self.SYSTEM_JLT2)
-        elif system == "JR - ACCAS":
-            self.systems.add(self.SYSTEM_ACCAS)
-        elif system == "tjs":
-            self.systems.add(self.SYSTEM_TJS)
-        else:
+    def __init__(self, pick_list, allowed_systems):
+        """Create a Pick instance."""
+        try:
+            system = self.SYSTEMS[pick_list[self.data_columns["system"]]]
+        except KeyError:
+            print(f"WARNING: Unknown system in CSV '{pick_list[self.data_columns['system']]}'.")
+            raise UnwantedSystemError(f"Unknown system '{pick_list[self.data_columns['system']]}'")
+
+        if system not in allowed_systems:
             raise UnwantedSystemError(f"Unknown system: {system}")
 
-        self.horse = pick_list[data_columns["horse"]]
-        self.course = pick_list[data_columns["course"]]
-        self.time = pick_list[data_columns["time"]]
-        self.date = pick_list[data_columns["date"]]
+        self.systems = {system}
+        self.horse = pick_list[self.data_columns["horse"]]
+        self.course = pick_list[self.data_columns["course"]]
+        self.time = pick_list[self.data_columns["time"]]
+        self.date = pick_list[self.data_columns["date"]]
 
     @property
     def system(self):
@@ -62,97 +85,122 @@ class Pick:
 
     @property
     def quantity(self):
+        """Return the amount of systems that chose this pick."""
         return len(self.systems)
 
     def __repr__(self):
         return f"{self.system} - {self.date} - {self.course} - {self.time} - {self.horse}"
 
-
-with open("ferret.csv") as input_csv:
-    picks_csv = csv.reader(input_csv)
-
-    first_line = ""
-    for line in picks_csv:
-        if not first_line:
-            first_line = line
-            continue    # Skip first line - column headers, not a pick
-        try:
-            picks.append(Pick(first_line, line))
-        except UnwantedSystemError:
-            continue    # Ignoring picks that aren't from our main EP systems.
-
-
-picks.sort(key=lambda p: (p.course, p.time))    # Sort picks by course then time
-
-
-picks_with_dupes = picks
-picks = []
-
-for single_pick in picks_with_dupes:
-    for pick in picks:
-        if single_pick.horse == pick.horse:
-            pick.systems.add(single_pick.system)
-            break
-    else:
-        picks.append(single_pick)
-
-
-num_doubles = len([pick for pick in picks if pick.quantity == 2])
-num_triples = len([pick for pick in picks if pick.quantity == 3])
-num_quadruples = len([pick for pick in picks if pick.quantity == 4])
-
-
-def get_row_fill_color(pick):
-    """Return the row fill color for the given pick."""
-    if {Pick.SYSTEM_MR3, Pick.SYSTEM_JLT2}.issubset(pick.systems):
-        return "#afc3fa"
-    elif Pick.SYSTEM_JLT2 in pick.systems:
-        return "#d5cff3"
-    elif Pick.SYSTEM_MR3 in pick.systems:
-        return "#adf4e7"
-    else:
+    @property
+    def row_fill_color(self):
+        """Return the row fill color for this pick."""
+        if {Pick.SYSTEM_MR3, Pick.SYSTEM_JLT2}.issubset(self.systems):
+            return "#afc3fa"
+        if Pick.SYSTEM_JLT2 in self.systems:
+            return "#d5cff3"
+        if Pick.SYSTEM_MR3 in self.systems:
+            return "#adf4e7"
         return "#cfe2f3"
 
+    def get_row_line_color(self, picks):
+        """Return the row line color for this pick."""
+        index = picks.index(self)
+        if index == 0:
+            return "#bbbbbb"
 
-def get_row_line_color(pick):
-    """Return the row line color for the given pick."""
-    index = picks.index(pick)
-    if index == 0:
+        previous_pick = picks[index - 1]
+        if self.course != previous_pick.course:
+            return "#000000"
+
         return "#bbbbbb"
 
-    previous_pick = picks[index - 1]
-    if pick.course != previous_pick.course:
-        return "#000000"
 
-    return "#bbbbbb"
+def create_picks_table(systems):
+    picks = []
+
+    with open("ferret.csv") as input_csv:
+        picks_csv = csv.reader(input_csv)
+
+        for line_number, line in enumerate(picks_csv):
+            if line_number == 0:
+                Pick.initialise_data_columns(line)
+                continue    # Skip first line - column headers, not a pick
+            try:
+                picks.append(Pick(line, systems))
+            except UnwantedSystemError:
+                continue    # Ignoring picks that aren't from our main EP systems.
+
+    picks.sort(key=lambda p: (p.course, p.time))    # Sort picks by course then time
+
+    picks_with_dupes = picks
+    picks = []
+
+    for single_pick in picks_with_dupes:
+        for pick in picks:
+            if single_pick.horse == pick.horse:
+                pick.systems.add(single_pick.system)
+                break
+        else:
+            picks.append(single_pick)
+
+    create_and_show_table(picks)
 
 
-fig = go.Figure(data=[go.Table(
-    columnwidth=[11, 40, 15, 40, 7],
-    columnorder=[1, 2, 3, 4, 5],
-    header=dict(
-        values=["<b>Sys</b>", "<b>Course</b>", "<b>Time</b>",
-                "<b>        Horse ({} total)<br>({} doub{}, {} tri{}, {} quad{})</b>"
-                .format(len(picks), num_doubles, "" if num_doubles == 1 else "s",
-                        num_triples, "" if num_triples == 1 else "s", num_quadruples, "" if num_quadruples == 1 else "s"),
-                "<b>x</b>"],
-        line_color="#bbbbbb", fill_color="#cfe2f3",
-        align="center", font=dict(color="black", size=12)
-    ),
-    cells=dict(
-        values=[
-            [pick.system for pick in picks],
-            [pick.course for pick in picks],
-            [pick.time for pick in picks],
-            [pick.horse for pick in picks],
-            [f"x{pick.quantity}" if pick.quantity > 1 else "" for pick in picks]
-        ],
-        line_color=[[get_row_line_color(pick) for pick in picks]],
-        fill_color=[[get_row_fill_color(pick) for pick in picks]],
-        align=["center", "left", "center", "left", "center"], font=dict(color="black", size=14),
-        height=22
-        ))
-])
+def get_multiples_text(picks, multiplier):
+    number = len([pick for pick in picks if pick.quantity == multiplier])
+    multiplier_in_words = {
+        2: "doub",
+        3: "tri",
+        4: "quad",
+        5: "quin",
+        6: "sex",
+    }[multiplier]
+    s = "" if number == 1 else "s"
+    return "{} {}{}, ".format(number, multiplier_in_words, s)
 
-fig.update_layout(width=650)
-fig.show()
+
+def create_and_show_table(picks):
+    multiples_text = "".join((get_multiples_text(picks, n) for n in range(2, 5)))[:-2]
+    fig = go.Figure(data=[go.Table(
+        columnwidth=[11, 40, 15, 40, 7],
+        columnorder=[1, 2, 3, 4, 5],
+        header=dict(
+            values=["<b>Sys</b>", "<b>Course</b>", "<b>Time</b>",
+                    "<b>        Horse ({} total)<br>({})</b>".format(len(picks), multiples_text),
+                    "<b>x</b>"],
+            line_color="#bbbbbb", fill_color="#cfe2f3",
+            align="center", font=dict(color="black", size=12)
+        ),
+        cells=dict(
+            values=[
+                [pick.system for pick in picks],
+                [pick.course for pick in picks],
+                [pick.time for pick in picks],
+                [pick.horse for pick in picks],
+                [f"x{pick.quantity}" if pick.quantity > 1 else "" for pick in picks]
+            ],
+            line_color=[[pick.get_row_line_color(picks) for pick in picks]],
+            fill_color=[[pick.row_fill_color for pick in picks]],
+            align=["center", "left", "center", "left", "center"], font=dict(color="black", size=14),
+            height=22
+            ))
+    ])
+
+    fig.update_layout(width=650)
+    fig.show()
+
+
+if __name__ == "__main__":
+    ep_systems = [Pick.SYSTEM_DTR, Pick.SYSTEM_MR3, Pick.SYSTEM_LT6R,
+                  Pick.SYSTEM_ACCAS, Pick.SYSTEM_TJS, Pick.SYSTEM_JLT2]
+    create_picks_table(ep_systems)         # ep6
+    ep_systems.remove(Pick.SYSTEM_JLT2)
+    create_picks_table(ep_systems)         # ep5
+    ep_systems.remove(Pick.SYSTEM_ACCAS)
+    ep_systems.remove(Pick.SYSTEM_TJS)
+    create_picks_table(ep_systems)         # ep3
+    bsp_systems = [Pick.SYSTEM_PACE, Pick.SYSTEM_EASED, Pick.SYSTEM_TOP_OF_POWER,
+                   Pick.SYSTEM_TOP_SPEED_AND_JOCKEY, Pick.SYSTEM_NTF_CHELTENHAM_1,
+                   Pick.SYSTEM_NTF_CHELTENHAM_2, Pick.SYSTEM_DTR_HURDLES_ONLY,
+                   Pick.SYSTEM_SPTT, Pick.SYSTEM_SPTJ]
+    create_picks_table(bsp_systems)
